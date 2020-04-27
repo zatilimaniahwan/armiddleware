1c14d4ea54af79000cf2520c900e2f00dcdaeeb2
Nurzatilimani Muhamad Ahwan <zatilimaniahwan@gmail.com>
2020-04-28 03:08:29 +0800

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,11 +4,15 @@ const User = require('../models/UserModel');
 const Bcrypt = require('bcryptjs'); // encrypt password
 
 // list all users
-exports.list = function (res) {
+exports.list = function (req,res) {
     User.getAllUser(function (err, users) {
         try {
-            res.send(users);
-            res.status(200).send({ error: false, message: 'Successful fetched' });
+            if(users.length >0){
+                res.send(users);
+                res.status(200).send({ error: false, message: 'Successful fetched' });
+            }else{
+                res.status(200).send({error: false, message: 'No data available'})
+            }
         }catch (err) {
             res.status(400).send({ error: true, message: err });
         }
@@ -24,9 +28,13 @@ exports.create = function (req, res) {
     if (!new_user.fullname || !new_user.username || !new_user.password || !new_user.email || !new_user.contact_number) {
         res.status(400).send({ error: true, message: 'Please provide the data' });
     } else {
-        User.createUser(new_user, function () {
+        User.createUser(new_user, function (res) {
             try {
-                res.status(200).send({ error: false, message: 'Successful created' });
+                if(res.length >0){
+                    res.status(200).send({ error: false, message: 'Successful created' });
+                }else{
+                    res.status(400).send({ error: false, message: 'Unsuccessful created' });
+                }
             } catch (err) {
                 res.status(400).send({ error: true, message: err });
             }
