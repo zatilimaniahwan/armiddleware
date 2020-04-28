f9001ee3161d874ba5284754995b4c1d0448734e
Nurzatilimani Muhamad Ahwan <zatilimaniahwan@gmail.com>
2020-04-28 22:47:03 +0800

    Update function create new user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -21,9 +21,17 @@ exports.list = function (req,res) {
 
 // create new user
 exports.create = function (req, res) {
-    // Encrypt password before save it to the database
-    req.body.password = Bcrypt.hashSync(req.body.password, 10);
-    var new_user = new User(req.body);
+    var new_user = new User({
+        'fullname': req.body.fullname,
+        'username': req.body.username,
+        'password': Bcrypt.hashSync(req.body.password, 10),  // Encrypt password before save it to the database
+        'email':req.body.email,
+        'contact_number':req.body.contact_number,
+        'role_id': req.body.role_id,
+        'is_active':1,
+        'activation_code':req.body.activation_code,
+        'current_period':req.body.current_period
+    });
     //handles null error 
     if (!new_user.fullname || !new_user.username || !new_user.password || !new_user.email || !new_user.contact_number) {
         res.status(400).send({ error: true, message: 'Please provide the data' });
